84b107fba1b02533d33a8e4bbcc9889070b2d19a
Marie-Dessons <marie.dessons@oclock.school>
2024-07-12 12:39:07 +0200

    Amélioration design et marge + modifications team modal

--- front/assets/js/team.js
+++ front/assets/js/team.js
@@ -5,9 +5,9 @@ async function showAllTeams() {
     const teamTemplate = document.getElementById("team-list-item");
     const teamContainer = document.getElementById("team-container");
     teamContainer.textContent = "";
-
+    
     const allTeams = await api.fetchAllTeams();
-
+    
     allTeams.forEach((team) => {
         const clone = document.importNode(teamTemplate.content, true);
         const teamName = clone.querySelector(".team-name");
@@ -20,28 +20,27 @@ async function showAllTeams() {
         teamBtn.addEventListener("click", getTeamModal);
         teamContainer.appendChild(clone);
     });
-
+    
 }
 async function getTeamModal(event) {
     event.preventDefault();
-
+    
     const teamId = event.currentTarget.dataset.id;
     const teamModal = document.querySelector("#team_modal");
     teamModal.classList.add("is-active");
-
+    
     const team = await api.fetchOneTeam(teamId);
     teamModal.dataset.id = teamId;
-
+    
     teamModal.querySelector(".team_name").textContent = team.name;
     teamModal.querySelector(".team_description").textContent = team.description;
-
+    
     const templatePkmRow = document.getElementById("template-row-table");
     const tablePkm = document.querySelector(".table");
-    tablePkm.textContent = "";
     
+    tablePkm.textContent = "";
     team.pokemons.forEach((pokemon) => {
         const clone = document.importNode(templatePkmRow.content, true);
-
         const numPkm = clone.querySelector('[slot="id"]').textContent = pokemon.id;
         const namePkm = clone.querySelector('[slot="name"]').textContent = pokemon.name;
         const hpPkm = clone.querySelector(' [slot="hp"]').textContent = pokemon.hp;
@@ -50,10 +49,8 @@ async function getTeamModal(event) {
         const atkSpePkm = clone.querySelector(' [slot="atk_spe"]').textContent = pokemon.atk_spe;
         const defSpePkm = clone.querySelector(' [slot="def_spe"]').textContent = pokemon.def_spe;
         const vitPkm = clone.querySelector(' [slot="speed"]').textContent = pokemon.speed;
-
-
+        
         tablePkm.appendChild(clone)
-
     })
 }
 
